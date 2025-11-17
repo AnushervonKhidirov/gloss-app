1e1aa659a73505882df9c9721c6abb84b8a385eb
AnushervonKhidirov <ankhid98@gmail.com>
2025-11-17 08:54:52 +0500

    updated

--- app/sign-in/index.tsx
+++ app/sign-in/index.tsx
@@ -1,11 +1,51 @@
-import SignInForm from '@components/form/sign-in-form';
 import { Link } from 'expo-router';
 import { StyleSheet, View } from 'react-native';
 
+import { Button, Form, Input } from '@ant-design/react-native';
+import InputPassword from '@components/input/input-password';
+import { AuthService } from '@services/auth.service';
+import { SignIn } from '@type/auth.type';
+import { useState } from 'react';
+
+const authService = new AuthService();
+
 const SignInScreen = () => {
+  const [form] = Form.useForm();
+
+  const [loading, setLoading] = useState(false);
+
+  async function onFinish(value: SignIn) {
+    console.log(value);
+
+    setLoading(true);
+
+    const [token, err] = await authService.signIn(value);
+    if (err) {
+      console.log('err', err);
+    } else {
+      console.log('sign in', token);
+      await AuthService.setToken(token);
+    }
+
+    setLoading(false);
+  }
+
   return (
     <View style={styles.form_wrapper}>
-      <SignInForm />
+      <Form form={form} onFinish={onFinish}>
+        <Form.Item name="username" rules={[{ required: true, message: 'Введите логин' }]}>
+          <Input placeholder="Логин"></Input>
+        </Form.Item>
+
+        <InputPassword />
+
+        <Form.Item>
+          <Button type="primary" onPress={form.submit} loading={loading}>
+            Войти
+          </Button>
+        </Form.Item>
+      </Form>
+
       <Link href="/sign-up">Sign up</Link>
     </View>
   );
--- app/sign-up/index.tsx
+++ app/sign-up/index.tsx
@@ -1,12 +1,58 @@
-import SignUpForm from '@components/form/sign-up-form';
 import { Link } from 'expo-router';
 import { StyleSheet, View } from 'react-native';
 
+import type { CreateUser } from '@type/user.type';
+
+import { Button, Form, Input } from '@ant-design/react-native';
+import InputPassword from '@components/input/input-password';
+import { useState } from 'react';
+
+import { AuthService } from '@services/auth.service';
+
+const authService = new AuthService();
+
 const SignUpScreen = () => {
+  const [form] = Form.useForm();
+  const [loading, setLoading] = useState(false);
+
+  async function onFinish(value: CreateUser) {
+    setLoading(true);
+
+    const [token, err] = await authService.signUp(value);
+    if (err) {
+      console.log('err', err);
+    } else {
+      await AuthService.setToken(token);
+    }
+
+    setLoading(false);
+  }
+
   return (
     <View style={styles.form_wrapper}>
-      <SignUpForm />
-      <Link href='/sign-in'>Sign in</Link>
+      <Form form={form} onFinish={onFinish}>
+        <Form.Item name="firstName" rules={[{ required: true, message: 'Введите имя' }]}>
+          <Input placeholder="Имя"></Input>
+        </Form.Item>
+
+        <Form.Item name="lastName">
+          <Input placeholder="Фамилия"></Input>
+        </Form.Item>
+
+        <Form.Item name="username" rules={[{ required: true, message: 'Введите логин' }]}>
+          <Input placeholder="Логин"></Input>
+        </Form.Item>
+
+        <InputPassword />
+
+        <Form.Item>
+          <Button type="primary" onPress={form.submit} loading={loading}>
+            Зарегистрироваться
+          </Button>
+        </Form.Item>
+      </Form>
+
+      <Link href="/sign-in">Sign in</Link>
     </View>
   );
 };
