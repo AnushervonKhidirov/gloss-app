94a07e8590b91e30c2fbf884b3b9c6341b5f4e2d
AnushervonKhidirov <ankhid98@gmail.com>
2025-11-17 21:28:36 +0500

    password input updated

--- components/input/input-password/index.tsx
+++ components/input/input-password/index.tsx
@@ -1,6 +1,7 @@
-import { Form, Input } from '@ant-design/react-native';
+import { Input } from '@ant-design/react-native';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 
 const InputPassword = () => {
   const [passwordVisible, setPasswordVisible] = useState(false);
@@ -10,22 +11,35 @@ const InputPassword = () => {
   }
 
   return (
-    <Form.Item
-      name="password"
-      rules={[{ required: true, message: 'Введите пароль' }]}
-      extra={
-        <AntDesign
-          style={{ justifyContent: 'center' }}
-          name={passwordVisible ? 'eye-invisible' : 'eye'}
-          size={24}
-          color="#ccc"
-          onPress={passwordVisibilityHandler}
-        />
-      }
-    >
-      <Input type={passwordVisible ? 'text' : 'password'} placeholder="Пароль"></Input>
-    </Form.Item>
+    <View style={style.container}>
+      <Input
+        type={passwordVisible ? 'text' : 'password'}
+        style={style.input}
+        placeholder="Пароль"
+      ></Input>
+      <AntDesign
+        style={style.icon}
+        name={passwordVisible ? 'eye-invisible' : 'eye'}
+        size={24}
+        color="#ccc"
+        onPress={passwordVisibilityHandler}
+      />
+    </View>
   );
 };
+
+const style = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+  },
+
+  input: {
+    flex: 1,
+  },
+
+  icon: {
+    alignSelf: 'center',
+  },
+});
 
 export default InputPassword;
