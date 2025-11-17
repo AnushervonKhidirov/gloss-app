616e9f595a715fed34bdc72baa2fb02bde049c75
AnushervonKhidirov <ankhid98@gmail.com>
2025-11-17 16:56:09 +0500

    refresh token updated

--- core/api/apiClient.ts
+++ core/api/apiClient.ts
@@ -1,26 +1,7 @@
-import axios, { AxiosError, AxiosRequestConfig } from 'axios';
+import axios from 'axios';
 
+import { isHttpException } from '@helper/error-handler';
 import { AuthService } from '@services/auth.service';
-
-type AxiosRequestConf = AxiosRequestConfig & { _retry: boolean };
-
-let isRefreshing = false;
-let failedQueue: Array<{
-  resolve: (value: string) => void;
-  reject: (reason?: any) => void;
-}> = [];
-
-const processQueue = (error: AxiosError | null, token: string | null = null): void => {
-  failedQueue.forEach(prom => {
-    if (error) {
-      prom.reject(error);
-    } else {
-      prom.resolve(token as string);
-    }
-  });
-
-  failedQueue = [];
-};
 
 const apiClient = axios.create({
   baseURL: 'http://192.168.0.93:4000/',
@@ -31,64 +12,31 @@ const apiClient = axios.create({
 });
 
 apiClient.interceptors.request.use(async config => {
-  try {
-    const token = await AuthService.getToken();
-
-    if (token) config.headers.Authorization = `Bearer ${token.accessToken}`;
-    return config;
-  } catch (err) {
-    return Promise.reject(err);
-  }
+  const token = await AuthService.getToken();
+  if (token) config.headers.Authorization = `Bearer ${token.accessToken}`;
+  return config;
 });
 
-apiClient.interceptors.response.use(
-  response => response,
-  async (err: AxiosError) => {
-    const authService = new AuthService();
-    const originalRequest = err.config as AxiosRequestConf;
+apiClient.interceptors.response.use(async response => {
+  console.log('response (interceptors)', response.data);
+  const authService = new AuthService();
+  const originalRequest = response.config;
 
-    if (err.response?.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
+  if (isHttpException(response.data) && response.data.statusCode === 401) {
+    const token = await AuthService.getToken();
 
-      if (isRefreshing) {
-        return new Promise<void>(function (resolve, reject) {
-          failedQueue.push({
-            resolve: (token: string) => {
-              originalRequest.headers!.Authorization = `Bearer ${token}`;
-              resolve(apiClient(originalRequest) as any);
-            },
-            reject,
-          });
-        });
-      }
+    if (!token) return response;
 
-      isRefreshing = true;
+    const [newToken, refreshErr] = await authService.refreshToken(token?.refreshToken);
+    if (refreshErr) return response;
 
-      const token = await AuthService.getToken();
-      if (token) {
-        try {
-          console.log('refreshing token');
+    AuthService.setToken(newToken);
+    originalRequest.headers.Authorization = `Bearer ${newToken.accessToken}`;
 
-          const [newToken, err] = await authService.refreshToken(token.refreshToken);
-          if (err) throw new AxiosError(err.error);
+    return apiClient(originalRequest);
+  }
 
-          AuthService.setToken(newToken);
-          isRefreshing = false;
-          processQueue(null, newToken.accessToken);
-          originalRequest.headers!.Authorization = `Bearer ${newToken.accessToken}`;
-          return apiClient(originalRequest);
-        } catch (err) {
-          console.log(err);
-
-          isRefreshing = false;
-          processQueue(err as AxiosError, null);
-          return Promise.reject(err);
-        }
-      }
-    }
-
-    return Promise.reject(err);
-  },
-);
+  return response;
+});
 
 export default apiClient;
