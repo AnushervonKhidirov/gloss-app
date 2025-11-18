240b12fb7472d82014107b1319439065cd13899b
AnushervonKhidirov <ankhid98@gmail.com>
2025-11-18 21:52:02 +0500

    create service form added

--- components/form/create-service-form.tsx
+++ components/form/create-service-form.tsx
@@ -1,43 +1,68 @@
-import { Link } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
-
-import type { CreateUser } from '@type/user.type';
+import type { Category } from '@type/category.type';
+import type { CreateService, Service } from '@type/service.type';
+import type { FC } from 'react';
 
 import { Button, Form, Input } from '@ant-design/react-native';
-import InputPassword from '@components/input/input-password';
+import AutoSelect from '@components/input/autoselect';
 import { useState } from 'react';
-
+import { StyleSheet, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-const CreateServiceForm = () => {
+import { ServiceService } from '@services/service.service';
+
+type CreateServiceFormProps = { categories: Category[]; onSuccess: (service: Service) => void };
+
+const serviceService = new ServiceService();
+
+const CreateServiceForm: FC<CreateServiceFormProps> = ({ categories, onSuccess }) => {
   const [form] = Form.useForm();
   const [loading, setLoading] = useState(false);
 
-  async function onFinish(value: CreateUser) {
+  async function onFinish(value: CreateService) {
     setLoading(true);
 
-    
+    const [service, err] = await serviceService.create(value);
+
+    if (err) {
+      console.log(err);
+    } else {
+      onSuccess(service);
+    }
 
     setLoading(false);
   }
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.form_wrapper}>
         <Form form={form} onFinish={onFinish}>
-          <Form.Item name="firstName" rules={[{ required: true, message: 'Введите имя' }]}>
-            <Input placeholder="Имя"></Input>
+          <Form.Item name="name" rules={[{ required: true, message: 'Введите название' }]}>
+            <Input placeholder="Название"></Input>
           </Form.Item>
 
-          <Form.Item name="lastName">
-            <Input placeholder="Фамилия"></Input>
+          <Form.Item
+            name="price"
+            getValueFromEvent={e => Number.parseInt(e.nativeEvent.text)}
+            rules={[{ required: true, message: 'Введите Цена' }]}
+          >
+            <Input type="number" placeholder="Цена"></Input>
           </Form.Item>
 
-          <Form.Item name="username" rules={[{ required: true, message: 'Введите логин' }]}>
-            <Input placeholder="Логин"></Input>
+          <Form.Item
+            name="duration"
+            getValueFromEvent={e => Number.parseInt(e.nativeEvent.text)}
+            rules={[{ required: true, message: 'Введите продолжительность' }]}
+          >
+            <Input type="number" placeholder="Продолжительность"></Input>
           </Form.Item>
 
-          <InputPassword />
+          <Form.Item name="categoryId" rules={[{ required: true, message: 'Выберите категорию' }]}>
+            <AutoSelect name="categoryId" placeholder="Категория" items={categories}></AutoSelect>
+          </Form.Item>
+
+          <Form.Item name="desc">
+            <Input.TextArea placeholder="Описание"></Input.TextArea>
+          </Form.Item>
 
           <Form.Item>
             <Button type="primary" onPress={form.submit} loading={loading}>
@@ -45,10 +70,6 @@ const CreateServiceForm = () => {
             </Button>
           </Form.Item>
         </Form>
-
-        <Link replace href="/sign-in">
-          Sign in
-        </Link>
       </View>
     </SafeAreaView>
   );
@@ -56,8 +77,7 @@ const CreateServiceForm = () => {
 
 const styles = StyleSheet.create({
   form_wrapper: {
-    display: 'flex',
-    height: '100%',
+    flex: 1,
     justifyContent: 'center',
   },
 });
