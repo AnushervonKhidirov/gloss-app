6e12422e0e466fed6225376a00387649420d3f76
AnushervonKhidirov <ankhid98@gmail.com>
2025-11-18 21:41:46 +0500

    autocomplete fixed

--- components/input/autoselect/index.tsx
+++ components/input/autoselect/index.tsx
@@ -1,16 +1,17 @@
 import type { FC } from 'react';
 
-import { useLayoutEffect, useRef, useState } from 'react';
-
-import { Input, List } from '@ant-design/react-native';
+import { Form, Input, List } from '@ant-design/react-native';
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { Keyboard, Pressable, ScrollView, StyleSheet, View } from 'react-native';
 import { Portal } from 'react-native-paper';
 
 type AutoSelectProps = {
   items: AutoSelectItem[];
   placeholder?: string;
+  name: string;
+  getBy?: 'id' | 'value';
   defaultSelected?: AutoSelectItem;
-  onSelect?: (item: AutoSelectItem) => void;
+  onSelect?: (item: AutoSelectItem | null) => void;
 };
 
 type Coordinates = {
@@ -27,6 +28,8 @@ type AutoSelectItem = { id: number; value: string };
 const AutoSelect: FC<AutoSelectProps> = ({
   items,
   placeholder,
+  name,
+  getBy = 'id',
   defaultSelected = null,
   onSelect,
 }) => {
@@ -34,15 +37,34 @@ const AutoSelect: FC<AutoSelectProps> = ({
   const scrollerRef = useRef<View>(null);
 
   const [show, setShow] = useState(false);
+  const [inputFocused, setInputFocused] = useState(false);
+  const [listFocused, setListFocused] = useState(false);
+  const listFocusTimeout = useRef<number>(null);
+
+  const [inputValue, setInputValue] = useState('');
+
   const [selected, setSelected] = useState<AutoSelectItem | null>(defaultSelected);
   const [filteredList, setFilteredList] = useState<AutoSelectItem[]>(items);
 
   const [coordinates, setCoordinates] = useState<Coordinates | null>(null);
   const [scrollCoordinates, setScrollCoordinates] = useState<Coordinates | null>(null);
 
-  function select(item: AutoSelectItem) {
+  const form = Form.useFormInstance();
+
+  function select(item: AutoSelectItem | null) {
     setSelected(item);
+
+    if (item) {
+      const value = getBy === 'id' ? item.id : item.value;
+      form.setFieldValue(name, value);
+      setInputValue(item.value);
+    } else {
+      form.setFieldValue(name, undefined);
+    }
+
     if (onSelect) onSelect(item);
+
+    setShow(false);
   }
 
   function find(value: string) {
@@ -62,6 +84,14 @@ const AutoSelect: FC<AutoSelectProps> = ({
     });
   }
 
+  function inputValueHandler(e: any) {
+    const value = e.target.value;
+    setInputValue(value);
+    find(value);
+
+    if (value === '') setSelected(null);
+  }
+
   useLayoutEffect(() => {
     getCoordinates();
 
@@ -74,14 +104,26 @@ const AutoSelect: FC<AutoSelectProps> = ({
     };
   }, []);
 
+  useEffect(() => {
+    setTimeout(getCoordinates, 0);
+  }, [filteredList]);
+
+  useEffect(() => {
+    const shouldShowList = inputFocused || listFocused;
+
+    setShow(shouldShowList);
+
+    if (selected) setInputValue(selected.value);
+  }, [inputFocused, listFocused]);
+
   return (
     <View style={styles.wrapper} ref={wrapperRef}>
       <Input
-        value={selected?.value}
+        value={inputValue}
         placeholder={placeholder}
-        onChangeText={find}
-        onFocus={() => setShow(true)}
-        onBlur={() => setShow(false)}
+        onFocus={() => setInputFocused(true)}
+        onBlur={() => setInputFocused(false)}
+        onChange={inputValueHandler}
       />
 
       {show && (
@@ -94,8 +136,19 @@ const AutoSelect: FC<AutoSelectProps> = ({
                 coordinates && scrollCoordinates ? coordinates.pageY - scrollCoordinates.height : 0,
             }}
           >
-            <ScrollView>
-              <List>
+            <ScrollView
+              onScroll={() => {
+                if (listFocusTimeout.current) clearTimeout(listFocusTimeout.current);
+                listFocusTimeout.current = setTimeout(() => setListFocused(false), 1500);
+              }}
+            >
+              <List
+                onTouchStart={() => {
+                  setListFocused(true);
+                  if (listFocusTimeout.current) clearTimeout(listFocusTimeout.current);
+                  listFocusTimeout.current = setTimeout(() => setListFocused(false), 1000);
+                }}
+              >
                 {filteredList.map(item => (
                   <List.Item key={item.id + item.value}>
                     <Pressable onPress={() => select(item)}>{item.value}</Pressable>
