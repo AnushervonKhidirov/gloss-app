625c4d97e5dd310710bc779426a0155247e3bbb8
AnushervonKhidirov <ankhid98@gmail.com>
2025-11-26 21:32:54 +0500

    workers added

--- app/private/workers.tsx
+++ app/private/workers.tsx
@@ -1,10 +1,83 @@
-import { Text, View } from 'react-native';
+import type { User } from '@type/user.type';
+
+import LoadingView from '@commonComponent/loading-view';
+import WorkerList from '@component/worker/worker-list';
+import useUserStore from '@store/users.store';
+import { useEffect, useState } from 'react';
+import { Alert, Text } from 'react-native';
+
+import userService from '@services/user.service';
 
 const WorkersScreen = () => {
+  const { users, setUsers, updateUser } = useUserStore(state => state);
+  const [loading, setLoading] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+
+  async function fetchUsers(refreshing: boolean = false) {
+    if (refreshing) {
+      setRefreshing(true);
+    } else {
+      setLoading(true);
+    }
+
+    const [users, err] = await userService.findMany();
+    if (err) {
+      Alert.alert('Ошибка');
+    } else {
+      setUsers(users);
+    }
+
+    setLoading(false);
+    setRefreshing(false);
+  }
+
+  async function approveUser(user: User) {
+    const [approvedUser, err] = await userService.approve(user);
+
+    if (err) {
+      Alert.alert('Ошибка');
+    } else {
+      updateUser(approvedUser);
+    }
+  }
+
+  async function archiveUser(user: User) {
+    const [archivedUser, err] = await userService.archive(user);
+
+    if (err) {
+      Alert.alert('Ошибка');
+    } else {
+      updateUser(archivedUser);
+    }
+  }
+
+  async function unarchiveUser(user: User) {
+    const [archivedUser, err] = await userService.unArchive(user);
+
+    if (err) {
+      Alert.alert('Ошибка');
+    } else {
+      updateUser(archivedUser);
+    }
+  }
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
-    <View>
-      <Text>Workers Screen (Tabs)</Text>
-    </View>
+    <LoadingView loading={loading}>
+      <WorkerList
+        workers={users}
+        refreshing={refreshing}
+        onRefresh={fetchUsers}
+        approve={approveUser}
+        archive={archiveUser}
+        unarchive={unarchiveUser}
+      >
+        <Text style={{ fontSize: 20, fontWeight: 700 }}>Ваши сотрудники</Text>
+      </WorkerList>
+    </LoadingView>
   );
 };
 
---
+++ component/worker/worker-list.tsx
@@ -0,0 +1,185 @@
+import type { User } from '@type/user.type';
+import type { ComponentProps, FC, PropsWithChildren } from 'react';
+
+import { Button, Card } from '@ant-design/react-native';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import useUserStore from '@store/user.store';
+import { Role } from '@type/user.type';
+import { RefreshControl, ScrollView, StyleSheet, Text, View } from 'react-native';
+
+import { gray, green, orange, red } from '@ant-design/colors';
+
+type WorkerListProps = PropsWithChildren<{
+  workers: User[];
+  emptyMessage?: string;
+  refreshing: boolean;
+  onRefresh: () => Promise<void>;
+  approve: (user: User) => Promise<void>;
+  archive: (user: User) => Promise<void>;
+  unarchive: (user: User) => Promise<void>;
+}>;
+
+type WorkerItemProps = {
+  worker: User;
+  approve: (user: User) => Promise<void>;
+  archive: (user: User) => Promise<void>;
+  unarchive: (user: User) => Promise<void>;
+};
+
+type WorkerItemHeaderTextProps = {
+  name: string;
+  role: Role;
+};
+
+type WorkerActionButtonsProps = {
+  worker: User;
+  approve: (user: User) => Promise<void>;
+  archive: (user: User) => Promise<void>;
+  unarchive: (user: User) => Promise<void>;
+};
+
+const WorkerList: FC<WorkerListProps> = ({
+  workers,
+  emptyMessage,
+  refreshing = false,
+  onRefresh,
+  approve,
+  archive,
+  unarchive,
+  children,
+}) => {
+  const message = emptyMessage ?? 'У вас пока нет работников';
+
+  return (
+    <View style={styles.container}>
+      <View>{children}</View>
+
+      <ScrollView
+        style={{ marginBottom: 43 }}
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+      >
+        <View style={styles.list}>
+          {workers.length > 0 ? (
+            workers.map(worker => (
+              <WorkerItem
+                key={worker.id}
+                worker={worker}
+                approve={approve}
+                archive={archive}
+                unarchive={unarchive}
+              />
+            ))
+          ) : (
+            <Text>{message}</Text>
+          )}
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+const WorkerItem: FC<WorkerItemProps> = ({ worker, approve, archive, unarchive }) => {
+  const icons: {
+    [key in 'verified' | 'notVerified' | 'archived']: {
+      name: ComponentProps<typeof MaterialCommunityIcons>['name'];
+      color: string;
+    };
+  } = {
+    verified: { name: 'account-check', color: green.primary! },
+    notVerified: { name: 'account-clock', color: orange[3] },
+    archived: { name: 'account-cancel', color: red.primary! },
+  };
+
+  let icon = icons.notVerified;
+  if (worker.verified) icon = icons.verified;
+  if (worker.archived) icon = icons.archived;
+
+  return (
+    <Card>
+      <Card.Header
+        title={<WorkerItemHeaderText name={worker.firstName} role={worker.role} />}
+        extra={
+          <View style={{ alignSelf: 'flex-end' }}>
+            <MaterialCommunityIcons name={icon.name} size={25} color={icon.color} />
+          </View>
+        }
+      />
+      <Card.Footer
+        content={
+          <WorkerActionButtons
+            worker={worker}
+            approve={approve}
+            archive={archive}
+            unarchive={unarchive}
+          />
+        }
+      />
+    </Card>
+  );
+};
+
+const WorkerItemHeaderText: FC<WorkerItemHeaderTextProps> = ({ name, role }) => {
+  return (
+    <View>
+      <Text style={{ fontSize: 17 }}>{name}</Text>
+      <Text style={{ color: gray[2] }}>{role}</Text>
+    </View>
+  );
+};
+
+const WorkerActionButtons: FC<WorkerActionButtonsProps> = ({
+  worker,
+  approve,
+  archive,
+  unarchive,
+}) => {
+  const user = useUserStore(state => state.user);
+
+  return user?.role === Role.ADMIN && worker.role === Role.WORKER ? (
+    <View style={styles.actionButtonsWrapper}>
+      {worker.archived ? (
+        <Button type="primary" size="small" onPress={() => unarchive(worker)}>
+          Восстановить
+        </Button>
+      ) : (
+        <Button type="warning" size="small" onPress={() => archive(worker)}>
+          Уволить нахуй!
+        </Button>
+      )}
+
+      {!worker.verified && (
+        <Button
+          type="primary"
+          style={{ backgroundColor: green.primary, borderColor: green.primary }}
+          size="small"
+          onPress={() => approve(worker)}
+        >
+          Подтвертидь
+        </Button>
+      )}
+    </View>
+  ) : (
+    <View />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    gap: 20,
+    flexDirection: 'column',
+    boxSizing: 'border-box',
+  },
+  list: {
+    gap: 10,
+    flex: 1,
+  },
+  actionButtonsWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    gap: 10,
+  },
+});
+
+export default WorkerList;
