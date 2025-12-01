04eeff6b14708a588ead383334c15ac097ba2f99
AnushervonKhidirov <ankhid98@gmail.com>
2025-12-01 17:45:00 +0500

    selectable service list added

---
+++ app/(main)/service/selected.tsx
@@ -0,0 +1,111 @@
+import type { SelectedService, Service } from '@type/service.type';
+
+import serviceService from '@service/service.service';
+import useServiceStore from '@store/service.store';
+import { useLayoutEffect, useState } from 'react';
+
+import LoadingView from '@commonComponent/loading-view';
+import ServiceList from '@component/service/service-list';
+import useUserStore from '@store/user.store';
+import { Alert } from 'react-native';
+
+import { Button } from '@ant-design/react-native';
+import Modal from '@commonComponent/modal';
+import SelectableServiceList from '@component/service/selectable-service-list';
+import { alertError } from '@helper/error-handler';
+
+function convertSelectedService(selectedServices: SelectedService[]): Service[] {
+  return selectedServices.map(workerService => {
+    return {
+      ...workerService.service,
+      price: workerService.price ?? workerService.service.price,
+    };
+  });
+}
+
+const SelectedServiceScreen = () => {
+  const user = useUserStore(state => state.user);
+
+  const { services, selectedServices, setServices } = useServiceStore(state => state);
+  const [selectServicesModalVisible, setSelectServicesModalVisible] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  function pushWorkerServices(selectedServices: SelectedService[]) {
+    setServices({ selectedServices });
+    setSelectServicesModalVisible(false);
+  }
+
+  function openSelectServiceModal() {
+    if (services.length === 0) {
+      Alert.alert('Нет созданных услуг', 'Сначала создайте услуги в разделе "Все услуги"');
+      return;
+    }
+
+    setSelectServicesModalVisible(true);
+  }
+
+  async function fetchOnLoad() {
+    setLoading(true);
+    await fetchData();
+    setLoading(false);
+  }
+
+  async function fetchData() {
+    await fetchServices();
+    await fetchSelectedServices();
+  }
+
+  async function fetchServices() {
+    const [services, err] = await serviceService.findMany();
+
+    if (err) {
+      alertError(err);
+    } else {
+      setServices({ services });
+    }
+  }
+
+  async function fetchSelectedServices() {
+    const [services, err] = await serviceService.findManySelected({ userId: user?.id });
+
+    if (err) {
+      alertError(err);
+    } else {
+      setServices({ selectedServices: services });
+    }
+  }
+
+  useLayoutEffect(() => {
+    fetchOnLoad();
+  }, []);
+
+  return (
+    <LoadingView loading={loading}>
+      <ServiceList
+        services={convertSelectedService(selectedServices)}
+        onRefresh={fetchData}
+        keyExtractor={service => `selected-list-${service.id}`}
+        emptyMessage="У вас пока нет выбранных услуг"
+      >
+        <Button type="primary" onPress={openSelectServiceModal}>
+          {selectedServices.length === 0 ? 'Выбрать услуги' : 'Редактировать'}
+        </Button>
+      </ServiceList>
+
+      <Modal
+        title="Выбор услуг"
+        isOpen={selectServicesModalVisible}
+        close={() => setSelectServicesModalVisible(false)}
+      >
+        <SelectableServiceList
+          services={services}
+          selectedList={selectedServices}
+          onSuccess={pushWorkerServices}
+          onRefresh={fetchData}
+        />
+      </Modal>
+    </LoadingView>
+  );
+};
+
+export default SelectedServiceScreen;
---
+++ component/service/selectable-service-list.tsx
@@ -0,0 +1,92 @@
+import type { CreateUpdateWorkerService, SelectedService, Service } from '@type/service.type';
+import type { FC } from 'react';
+
+import { Button } from '@ant-design/react-native';
+import ScrollView from '@commonComponent/scroll-view';
+import { useState } from 'react';
+import { Alert, View } from 'react-native';
+import SelectedServiceCard from './selected-service-card';
+
+import serviceService from '@service/service.service';
+
+type SelectableServiceListProps = {
+  services: Service[];
+  selectedList: SelectedService[];
+  onSuccess: (workerServices: SelectedService[]) => void;
+  onRefresh: () => Promise<void>;
+};
+
+const SelectableServiceList: FC<SelectableServiceListProps> = ({
+  services,
+  selectedList,
+  onSuccess,
+  onRefresh,
+}) => {
+  const [loading, setLoading] = useState(false);
+  const [selectedServices, setSelectedServices] = useState<Service[]>(
+    selectedList.map(selected => selected.service),
+  );
+
+  async function submit() {
+    setLoading(true);
+
+    const workerServices: CreateUpdateWorkerService[] = selectedServices.map(selectedService => {
+      const originalService = services.find(service => service.id === selectedService.id);
+      const price = originalService?.price === selectedService.price ? null : selectedService.price;
+
+      if (price) return { serviceId: selectedService.id, price: price };
+      return { serviceId: selectedService.id };
+    });
+
+    const [newWorkerServices, err] = await serviceService.selectedHandler(workerServices);
+
+    if (err) {
+      Alert.alert('Ошибка', 'Что-то пошло не так, пожалуйста повторите позже');
+    } else {
+      onSuccess(newWorkerServices);
+    }
+
+    setLoading(false);
+  }
+
+  function selectHandler(service: Service, selected: boolean) {
+    const newServices = [...selectedServices];
+    const existingIndex = newServices.findIndex(serviceItem => serviceItem.id === service.id);
+
+    if (existingIndex === -1 && selected) {
+      newServices.push(service);
+      setSelectedServices(newServices);
+    }
+
+    if (existingIndex !== -1 && selected) {
+      newServices[existingIndex] = service;
+      setSelectedServices(newServices);
+    }
+
+    if (existingIndex !== -1 && !selected) {
+      newServices.splice(existingIndex, 1);
+      setSelectedServices(newServices);
+    }
+  }
+
+  return (
+    <View style={{ flex: 1, gap: 16 }}>
+      <ScrollView onRefresh={onRefresh}>
+        {services.map(service => (
+          <SelectedServiceCard
+            key={`selected-${service.id}`}
+            service={service}
+            selectedService={selectedList.find(selected => selected.serviceId === service.id)}
+            onSelect={selectHandler}
+          />
+        ))}
+      </ScrollView>
+
+      <Button type="primary" loading={loading} onPress={submit}>
+        Сохранить
+      </Button>
+    </View>
+  );
+};
+
+export default SelectableServiceList;
