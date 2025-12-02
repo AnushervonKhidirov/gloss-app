af9c1d95c37c4985fde3ab60d251c61b45e4edc6
AnushervonKhidirov <ankhid98@gmail.com>
2025-12-02 22:26:33 +0500

    profile added (without functionality)

--- app/(main)/profile/index.tsx
+++ app/(main)/profile/index.tsx
@@ -1,13 +1,32 @@
-import { useRouter } from 'expo-router';
-import { View } from 'react-native';
+import { Role } from '@type/user.type';
+import type { FC } from 'react';
 
+import useUserStore from '@store/user.store';
+import { useRouter } from 'expo-router';
+import { useState } from 'react';
+
+import { Button } from '@ant-design/react-native';
 import ButtonPrimary from '@commonComponent/button-primary';
+import { RefreshControl, ScrollView, StyleSheet, Text, View } from 'react-native';
+
 import authService from '@service/auth.service';
 import TokenService from '@service/token.service';
-import { useState } from 'react';
+import userService from '@service/user.service';
+
+import { grey } from '@constant/theme';
+import { alertError } from '@helper/error-handler';
+import parsePhoneNumber from 'libphonenumber-js';
+
+function getRole(role: Role) {
+  if (role === Role.ADMIN) return 'Администратор';
+  if (role === Role.WORKER) return 'Сотрудник';
+}
 
 const ProfileScreen = () => {
   const router = useRouter();
+
+  const { user, removeUser, setUser } = useUserStore(state => state);
+  const [refreshing, setRefreshing] = useState(false);
   const [loading, setLoading] = useState(false);
 
   async function signOut() {
@@ -16,17 +35,102 @@ const ProfileScreen = () => {
     if (token) await authService.signOut(token);
 
     await TokenService.removeToken();
+    removeUser();
     setLoading(false);
     router.navigate('/(auth)');
   }
 
+  async function refresh() {
+    setRefreshing(true);
+
+    const [user, err] = await userService.findMe();
+
+    if (err) {
+      alertError(err);
+    } else {
+      setUser(user);
+    }
+
+    setRefreshing(false);
+  }
+
   return (
-    <View>
-      <ButtonPrimary loading={loading} onPress={signOut}>
-        Выйти
-      </ButtonPrimary>
+    user && (
+      <View style={styles.profileWrap}>
+        <ScrollView
+          contentContainerStyle={{ flexGrow: 1 }}
+          style={{ flex: 1 }}
+          refreshControl={<RefreshControl refreshing={refreshing} onRefresh={refresh} />}
+        >
+          <View style={styles.profileContent}>
+            <ListItem field="Имя" value={user.firstName} />
+            <ListItem field="Фамилия" value={user.lastName} />
+            <ListItem field="Телефон" value={parsePhoneNumber(user.phone)?.formatNational()} />
+            <ListItem field="Логин" value={user.username} />
+            <ListItem field="Роль" value={getRole(user.role)} />
+            <ListItem field="Специальность" value={user.specialty?.name} inRow />
+          </View>
+        </ScrollView>
+
+        <View style={styles.actionButtonWrap}>
+          <ButtonPrimary loading={loading} style={styles.actionButton}>
+            Сменить пароль
+          </ButtonPrimary>
+
+          <ButtonPrimary loading={loading} style={styles.actionButton}>
+            Редактировать
+          </ButtonPrimary>
+        </View>
+
+        <Button type="warning" loading={loading} onPress={signOut}>
+          Выйти
+        </Button>
+      </View>
+    )
+  );
+};
+
+const ListItem: FC<{ field: string; value?: string | null; inRow?: boolean }> = ({
+  field,
+  value,
+  inRow,
+}) => {
+  return (
+    <View style={{ flexDirection: inRow ? 'column' : 'row' }}>
+      <Text style={{ flex: inRow ? 0 : 1, fontSize: 20 }}>
+        {field}
+        {!inRow && ':'}
+      </Text>
+      <Text style={{ fontSize: inRow ? 17 : 20, color: inRow ? grey[6] : undefined }}>
+        {value ?? '—'}
+      </Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  profileWrap: {
+    flex: 1,
+    gap: 16,
+  },
+
+  profileContent: {
+    flex: 1,
+    gap: 10,
+  },
+
+  actionButtonWrap: {
+    flexDirection: 'row',
+    gap: 16,
+  },
+
+  actionButton: {
+    flexGrow: 1,
+  },
+
+  listItem: {
+    flexDirection: 'row',
+  },
+});
 
 export default ProfileScreen;
