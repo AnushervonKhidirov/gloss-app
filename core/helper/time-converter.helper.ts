95b9420252dcab73d4266a33a880239d274bc43c
AnushervonKhidirov <ankhid98@gmail.com>
2025-12-05 10:54:41 +0500

    separate time by day time helper added

--- core/helper/time-converter.helper.ts
+++ core/helper/time-converter.helper.ts
@@ -1,3 +1,4 @@
+import type { DayTimes, SelectableTime } from '@type/time.type';
 import type { Dayjs } from 'dayjs';
 
 import dayjs from 'dayjs';
@@ -17,4 +18,22 @@ export function getDateString(date: Dayjs): string {
 
   if (dayText[diff]) return `${dayText[diff]} в ${date.format('H:mm')}`;
   return date.format('DD MMM в H:mm (dd)');
+}
+
+export function separateByDayTime(times: SelectableTime[]) {
+  const dayTimes: DayTimes = {
+    morning: { title: 'Утро', values: [] },
+    afternoon: { title: 'День', values: [] },
+    evening: { title: 'Вечер', values: [] },
+  };
+
+  for (const time of times) {
+    const h = time.value.get('hour');
+
+    if (h < 12) dayTimes.morning.values.push(time);
+    if (h >= 12 && h < 18) dayTimes.afternoon.values.push(time);
+    if (h >= 18) dayTimes.evening.values.push(time);
+  }
+
+  return dayTimes;
 }
