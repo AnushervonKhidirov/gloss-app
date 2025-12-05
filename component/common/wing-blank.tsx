7e830c023da2ea1b1da7604b7e08c2305ca19fec
AnushervonKhidirov <ankhid98@gmail.com>
2025-12-05 19:39:16 +0500

    style props added

--- component/common/wing-blank.tsx
+++ component/common/wing-blank.tsx
@@ -1,8 +1,12 @@
 import type { FC, PropsWithChildren } from 'react';
+import type { StyleProp, ViewStyle } from 'react-native';
 import { View } from 'react-native';
 
-const WingBlank: FC<PropsWithChildren> = ({ children }) => {
-  return <View style={{ paddingInline: 16 }}>{children}</View>;
+const WingBlank: FC<PropsWithChildren & { style?: StyleProp<ViewStyle> }> = ({
+  style = {} as {},
+  children,
+}) => {
+  return <View style={{ paddingInline: 16, ...style }}>{children}</View>;
 };
 
 export default WingBlank;
